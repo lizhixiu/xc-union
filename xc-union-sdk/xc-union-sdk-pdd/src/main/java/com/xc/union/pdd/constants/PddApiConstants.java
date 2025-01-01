b951b1a35186e0275ad2f4399ddbed50efbb403c
xiuj <82015742@qq.com>
2025-01-01 17:08:12 +0800

    feat:拼多多接口完善。

--- xc-union-sdk/xc-union-sdk-pdd/src/main/java/com/xc/union/pdd/constants/PddApiConstants.java
+++ xc-union-sdk/xc-union-sdk-pdd/src/main/java/com/xc/union/pdd/constants/PddApiConstants.java
@@ -1,6 +1,6 @@
 package com.xc.union.pdd.constants;
 
-import com.pdd.pop.sdk.http.api.pop.request.PddDdkGoodsRecommendGetRequest;
+import com.pdd.pop.sdk.http.api.pop.request.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +18,18 @@ public class PddApiConstants {
     static {
         //多多进宝商品推荐API
         REQUEST_MAP.put( "pdd.ddk.goods.recommend.get", PddDdkGoodsRecommendGetRequest.class );
+        //多多进宝商品详情查询
+        REQUEST_MAP.put( "pdd.ddk.goods.detail", PddDdkGoodsDetailRequest.class );
+        //多多进宝推广链接生成
+        REQUEST_MAP.put( "pdd.ddk.goods.promotion.url.generate", PddDdkGoodsPromotionUrlGenerateRequest.class );
 
+        //生成营销工具推广链接
+        REQUEST_MAP.put( "pdd.ddk.rp.prom.url.generate", PddDdkRpPromUrlGenerateRequest.class );
+        //生成商城推广链接
+        REQUEST_MAP.put( "pdd.ddk.cms.prom.url.generate", PddDdkCmsPromUrlGenerateRequest.class );
+
+        //单品推广转链
+        REQUEST_MAP.put( "pdd.ddk.goods.zs.unit.url.gen", PddDdkGoodsZsUnitUrlGenRequest.class );
     }
 
 }
