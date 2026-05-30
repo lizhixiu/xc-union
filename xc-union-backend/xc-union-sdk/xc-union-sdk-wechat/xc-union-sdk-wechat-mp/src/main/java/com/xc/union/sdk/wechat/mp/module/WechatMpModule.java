081c78c2cb8ea38092819b5b3f50779db35ac921
zhixiulee <82015742@qq.com>
2026-05-31 00:17:19 +0800

    feat:微信公众号模块调整。

--- xc-union-backend/xc-union-sdk/xc-union-sdk-wechat/xc-union-sdk-wechat-mp/src/main/java/com/xc/union/sdk/wechat/mp/module/WechatMpModule.java
+++ xc-union-backend/xc-union-sdk/xc-union-sdk-wechat/xc-union-sdk-wechat-mp/src/main/java/com/xc/union/sdk/wechat/mp/module/WechatMpModule.java
@@ -1,10 +1,10 @@
 package com.xc.union.sdk.wechat.mp.module;
 
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSON;
-import cn.hutool.json.JSONUtil;
 import com.xc.union.sdk.wechat.mp.config.WechatMpConfig;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
 import org.ssssssss.script.annotation.Comment;
 
 /**
@@ -13,43 +13,27 @@ import org.ssssssss.script.annotation.Comment;
 @Slf4j
 public class WechatMpModule {
 
-    private final WechatMpConfig config;
+    private final WxMpService wxMpService;
 
     public WechatMpModule(WechatMpConfig config) {
-        this.config = config;
-    }
+        WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
+        configStorage.setAppId(config.getAppId());
+        configStorage.setSecret(config.getAppSecret());
+        configStorage.setToken(config.getToken());
+        configStorage.setAesKey(config.getEncodingAESKey());
 
-    @Comment("获取微信 Access Token")
-    public JSON getAccessToken() {
-        if (config == null) {
-            throw new IllegalStateException("WechatMpConfig 未初始化！");
-        }
-
-        if (config.isDebug()) {
-            log.info("获取微信 Access Token, appId: {}", config.getAppId());
-        }
-
-        // TODO: 实现获取 Access Token 逻辑
-        return JSONUtil.parse("{}");
-    }
-
-    @Comment("微信通用接口")
-    public JSON execute(java.util.Map<String, String> params) {
-        if (config == null) {
-            throw new IllegalStateException("WechatMpConfig 未初始化！");
-        }
-
-        String apiMethodName = params.get("apiMethodName");
-        if (StrUtil.isBlank(apiMethodName)) {
-            throw new IllegalArgumentException("参数【apiMethodName】方法名称为空！");
-        }
+        WxMpService service = new WxMpServiceImpl();
+        service.setWxMpConfigStorage(configStorage);
+        this.wxMpService = service;
 
         if (config.isDebug()) {
-            log.info("请求参数：{}", JSONUtil.formatJsonStr(JSONUtil.toJsonStr(params)));
+            log.info("微信公众号模块初始化完成, appId: {}", config.getAppId());
         }
+    }
 
-        // TODO: 根据 apiMethodName 调用不同的微信接口
-        return JSONUtil.parse("{}");
+    @Comment("获取 WxMpService 实例")
+    public WxMpService me() {
+        return wxMpService;
     }
 
 }
